54c744bda91ffda80af06c7c1f262d5d70da8f6b
hahahhahahaahahahhaha <ist671@hotmail.com>
2026-09-15 05:51:12 +0800

    Update Firebase configuration for new project

--- public/firebase-init.js
+++ public/firebase-init.js
@@ -3,9 +3,9 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-
 
 const firebaseConfig = {
   apiKey: "AIzaSyBbgy76ThxHaKlOhpiLQ6Nw4ekR1b9t3DA",
-  authDomain: "s0lace-web.firebaseapp.com",
+  authDomain: "lunace-web.firebaseapp.com",
   databaseURL: "https://s0lace-web-default-rtdb.firebaseio.com",
-  projectId: "s0lace-web"
+  projectId: "lunace-web"
 };
 
 export const app =
